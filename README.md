b150abcef7eea29bcad0d869fa46324044e8ee3d
Vindhya Raviraj <me@vindhya.ca>
2017-03-20 23:08:14 -0400

    Completed homework in exercises.js and Feynman writing prompts in README.md

--- README.md
+++ README.md
@@ -5,11 +5,17 @@
 1. Feynman Writing Prompts - Write out explanations of the following concepts like you are explaining it to a 12 year old.  Doing this will help you quickly discover any holes in your understanding.  Ask your questions on Slack.
 		
 	* Callback Functions
+		* A function is called a callback function when it is passed into another function as an argument. 
 	* Closure
+		* Closures are a feature of JavaScipt where functions "remember" the scope where they were defined. For example, a function defined inside an outer function will have access to the outer functin's parameters.
 	* arguments array
+		* The `arguments` array allows access to additional parameters used when a function was called even if those parameters don't exist in the definition of the function.
 	* recursion
+		* Recursion is when a function calls itself. It's important to have a base case so that the function doesn't continue recursing forever.
 	* prototype
+		* A prototype is like a framework for an object - it defines the properties and methods which the object has.
 	* constructors
+		* Constructors are a type of function which create a new instance of an object when called upon.
 
 
 2. Fork and clone this repo.  When you need to commit use the following commands.
--- exercises.js
+++ exercises.js
@@ -6,27 +6,63 @@ function multiplyArguments() {
 	//use the arguments keyword to multiply all of the arguments together and return the product
 	//if no arguments are passed in return 0
 	//if one argument is passed in just return it
+
+	var product = 1;
+
+	if (arguments.length === 0) {
+		return 0;
+	} else if (arguments.length === 1) {
+		return arguments[0];
+	} else {
+		for (var i = 0; i < arguments.length; i++) {
+			product = product * arguments[i];
+		}
+		return product;
+	}
 }
 
 function invokeCallback(cb) {
 	//invoke cb
+
+	cb();
 }
 
 function sumArray(numbers, cb) {
 	//sum up all of the integers in the numbers array
 	//pass the result to cb
 	//no return is necessary
+
+	cb(numbers.reduce(function(sum, value) {
+		return sum + value;
+	}));
 }
 
 function forEach(arr, cb) {
 	//iterate over arr and pass its values to cb one by one
 	//hint: you will be invoking cb multiple times (once for each value in the array)
+
+	arr.forEach(function(value) {
+		cb(value);
+	});
+
+	// *** check if i'm actually supposed to use forEach for this solution ***
 }
 
 function map(arr, cb) {
 	//create a new array
 	//iterate over each value in arr, pass it to cb, then place the value returned from cb into the new arr
 	//the new array should be the same length as the array argument
+
+	var newArray;
+
+	newArray = arr.map(function(value) {
+		return cb(value);
+	});
+
+	// console.log(arr);
+	// console.log(newArray);
+
+	return newArray;
 }
 
 function getUserConstructor() {
@@ -36,11 +72,28 @@ function getUserConstructor() {
 	//the constructor should have a method 'sayHi' on its prototype that returns the string 'Hello, my name is {{name}}'
 	//{{name}} should be the name set on each instance
 	//return the constructor
+
+	function User(options) {
+		this.username = options.username;
+		this.name = options.name;
+		this.email = options.email;
+		this.password = options.password;
+	}
+
+	User.prototype.sayHi = function() {
+		return 'Hello, my name is ' + this.name;
+	};
+
+	return User;
 }
 
 function addPrototypeMethod(Constructor) {
 	//add a method to the constructor's prototype
 	//the method should be called 'sayHi' and should return the string 'Hello World!'
+
+	Constructor.prototype.sayHi = function() {
+		return 'Hello World!';
+	};
 }
 
 function addReverseString() {
@@ -48,6 +101,25 @@ function addReverseString() {
 	//name this method reverse
 	//hint:
 	//you will need to use 'this' inside of reverse
+
+	String.prototype.reverse = function() {
+		var reversedString = '';
+
+		// this.forEach(function(value, index) {
+		// 	reversedArray[this.length - index - 1] = value;
+		// });
+
+		// console.log(this.length);
+		// console.log(this[this.length - 1]);
+
+		for (var i = this.length - 1; i >= 0; i--) {
+			reversedString += this[i];
+		}
+
+		//console.log(reversedString);
+		
+		return reversedString;
+	};
 }
 
 function nFactorial(n) {
@@ -55,6 +127,14 @@ function nFactorial(n) {
 	//solve this recursively
 	//example:
 	//the factorial of 3 is 6 (3 * 2 * 1)
+
+	if (n === 2) {
+		return 2;
+	} else if (n === 1) {
+		return 1;
+	} else {
+		return n * nFactorial(n - 1);
+	}
 }
 
 function cacheFunction(cb) {
@@ -69,6 +149,17 @@ function cacheFunction(cb) {
 	//if the function you return is invoked with 5 it would pass 5 to cb(5) and return 25
 	//if the function you return is invoked again with 5 it will look on an object in the closure scope
 	//and return 25 directly and will not invoke cb again
+
+	var myCache = {};
+
+	return function(argument) {
+		if (myCache.hasOwnProperty(argument)) {
+			return myCache[argument];
+		} else {
+			myCache[argument] = cb(argument);
+			return myCache[argument];
+		}
+	};
 }
 
 
